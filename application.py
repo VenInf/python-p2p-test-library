a1c59ed0b420e00585f6f08997fdcd3f002a2d61
George Gerasev <george30032002@gmail.com>
2024-05-15 16:13:43 +0300

    application cleanup

--- application.py
+++ application.py
@@ -1,15 +1,6 @@
-#######################################################################################################################
-# Author: Maurice Snoeren                                                                                             #
-# Version: 0.1 beta (use at your own risk)                                                                            #
-#                                                                                                                     #
-# This example show how to derive a own Node class (MyOwnPeer2PeerNode) from p2pnet.Node to implement your own Node   #
-# implementation. See the MyOwnPeer2PeerNode.py for all the details. In that class all your own application specific  #
-# details are coded.                                                                                                  #
-#######################################################################################################################
-
 import sys
 import time
-sys.path.insert(0, '..') # Import the files where the modules are located
+sys.path.insert(0, '..') 
 
 from lib.main_node import MainNode
 from lib.regular_node import RegularNode
@@ -18,16 +9,12 @@ from lib.regular_node import RegularNode
 node_main = MainNode("127.0.0.1", 8001, id=1)
 node_2 = RegularNode("127.0.0.1", 8002, id=2, main_id=1, main_node_host="127.0.0.1", main_node_port=8001)
 node_3 = RegularNode("127.0.0.1", 8003, id=3, main_id=1, main_node_host="127.0.0.1", main_node_port=8001)
-node_4 = RegularNode("127.0.0.1", 8004, id=4, main_id=1, main_node_host="127.0.0.1", main_node_port=8001)
-
 
 time.sleep(1)
 
 node_main.start()
 node_2.start()
 node_3.start()
-node_4.start()
-
 
 time.sleep(1)
 
@@ -35,19 +22,16 @@ debug = True
 node_main.debug = debug
 node_2.debug = debug
 node_3.debug = debug
-node_4.debug = debug
-
 
 time.sleep(2)
 
-node_main.send_known_nodes()
+# node_main.send_known_nodes()
 
-time.sleep(2)
+# time.sleep(2)
 
 # node_2.connect_to_known_nodes()
 # node_3.connect_to_known_nodes()
-node_4.connect_to_known_nodes()
-time.sleep(2)
+# time.sleep(2)
 
 print("-------- Finish initialisation -------------")
 
@@ -58,23 +42,23 @@ node_3.add_data(101, " World!")
 print(f"node_2.known_data {node_2.known_data}")
 print(f"node_3.known_data {node_3.known_data}")
 
-node_4.request_known_data(node_2.id)
+node_main.request_known_data(node_2.id)
 time.sleep(2)
 
-node_4.request_known_data(node_3.id)
+node_main.request_known_data(node_3.id)
 time.sleep(2)
 
-print(f"node_4.data_id_table -- {node_4.data_id_table}")
-print(f"node_4.known_data -- {node_4.known_data}")
+print(f"node_main.data_id_table -- {node_main.data_id_table}")
+print(f"node_main.known_data -- {node_main.known_data}")
 
-node_4.request_data_by_id(100)
+node_main.request_data_by_id(100)
 time.sleep(2)
 
-node_4.request_data_by_id(101)
+node_main.request_data_by_id(101)
 time.sleep(2)
 
-print(f"node_4.data_id_table -- {node_4.data_id_table}")
-print(f"node_4.known_data -- {node_4.known_data}")
+print(f"node_main.data_id_table -- {node_main.data_id_table}")
+print(f"node_main.known_data -- {node_main.known_data}")
 
 
 
@@ -83,6 +67,4 @@ print("--------      End network      -------------")
 node_main.stop()
 node_2.stop()
 node_3.stop()
-node_4.stop()
-
 print('end test')
